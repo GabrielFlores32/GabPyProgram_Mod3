6264c26e04493f6c5fb4ce67b8757f0da3307765
Gabriel Flores <gabgabflores2@gmail.com>
2024-05-08 18:11:33 +0800

    ito na tlaga real

---
+++ basic_calculator.py
@@ -0,0 +1,14 @@
+#create a simple calculator app
+#adding the calcualtor app to a customtkinter
+
+
+import customtkinter
+import customtkinter as ctk
+import tkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.title("100% safe window promise") 
+app.geometry("500x500") 
