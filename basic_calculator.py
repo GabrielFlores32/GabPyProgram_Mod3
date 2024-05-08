84b4c0ceb3ed8d8576a1a1e786c009ff3803a50e
Gabriel Flores <gabgabflores2@gmail.com>
2024-05-08 18:34:30 +0800

    input box done

--- basic_calculator.py
+++ basic_calculator.py
@@ -7,10 +7,27 @@ import customtkinter as ctk
 from tkinter import *
 from tkinter import messagebox
 
+#gui theme
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
+#size of the window
 app = customtkinter.CTk()
 app.title("simple calculator app") 
-app.geometry("500x500") 
+app.geometry("300x300") 
+
+#result box
+main_entry = customtkinter.CTkEntry(app, width=280, height=50)
+main_entry.place(x=10, y=10)
+
+
+
+
+
+
+
+
+
+
+app.mainloop()
 
